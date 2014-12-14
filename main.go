85bbe272e6a5e4c76595405c4e5cf6890a05cb59
Gabriel Marquez <Gabriel@Conube.com.br>
2014-12-13 23:24:37 -0200

    now using builtin service

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
  
 import (
     "fmt"
-    "log"
+    "time"
+    "os"
+    "os/user"
+
     "encoding/json"
     "net/http"
     "io/ioutil"
-    "time"
+
+	"bitbucket.org/kardianos/service"
 )
 
 const (
@@ -22,46 +26,150 @@ type Settings struct {
 	Ip string
 }
 
+var log service.Logger
+
 func main() {
+	var name = "dyndns-dnsmadeeasy"
+	var displayName = "Dynamic DNS updater for DnsMadeEasy"
+	var desc = "Dynamic DNS updater for DnsMadeEasy"
 
-	// infity loop
-    for {
-	    settings := readSettings()
+	var s, err = service.NewService(name, displayName, desc)
+	log = s
 
-		settings.Ip = getExternalIp()
-
-		result := updateIp(settings.Username, settings.Password, settings.Id, settings.Ip)
-		fmt.Printf("%s\n", result)
-
-		saveSettings(settings)
-
-		// sleep based on interval settings
-		time.Sleep(time.Duration(settings.Interval) * time.Second)
+	if err != nil {
+		fmt.Printf("%s unable to start: %s", displayName, err)
+		return
 	}
 
+	if len(os.Args) > 1 {
+		var err error
+		verb := os.Args[1]
+		switch verb {
+		case "install":
+			err = s.Install()
+			if err != nil {
+				fmt.Printf("Failed to install: %s\n", err)
+				return
+			}
+			fmt.Printf("Service \"%s\" installed.\n", displayName)
+		case "remove":
+			err = s.Remove()
+			if err != nil {
+				fmt.Printf("Failed to remove: %s\n", err)
+				return
+			}
+			fmt.Printf("Service \"%s\" removed.\n", displayName)
+		case "run":
+			doWork()
+		case "start":
+			err = s.Start()
+			if err != nil {
+				fmt.Printf("Failed to start: %s\n", err)
+				return
+			}
+			fmt.Printf("Service \"%s\" started.\n", displayName)
+		case "stop":
+			err = s.Stop()
+			if err != nil {
+				fmt.Printf("Failed to stop: %s\n", err)
+				return
+			}
+			fmt.Printf("Service \"%s\" stopped.\n", displayName)
+		}
+		return
+	}
+	err = s.Run(func() error {
+		// start
+		go doWork()
+		return nil
+	}, func() error {
+		// stop
+		stopWork()
+		return nil
+	})
+	if err != nil {
+		s.Error(err.Error())
+	}
+}
+
+var exit = make(chan struct{})
+
+func doWork() {
+	log.Info("DnsMadeEasy updater is running")
+	fmt.Printf("DnsMadeEasy updater is running")
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+
+		    settings := readSettings()
+
+		    if settings.Id > 0 {
+				settings.Ip = getExternalIp()
+
+				result := updateIp(settings.Username, settings.Password, settings.Id, settings.Ip)
+				fmt.Printf("%s\n", result)
+			}
+
+			saveSettings(settings)
+
+			// sleep based on interval settings
+			time.Sleep(time.Duration(settings.Interval) * time.Minute)
+		case <-exit:
+			ticker.Stop()
+			return
+		}
+	}
+}
+
+func stopWork() {
+	log.Info("DnsMadeEasy updater is stopping!")
+	exit <- struct{}{}
 }
 
 func readSettings() Settings {
-	file, err := ioutil.ReadFile("settings.json")
-	if err != nil {
-		log.Fatal("ioutil.ReadFile", err)
-	}    
-    var settings Settings
-    err = json.Unmarshal(file, &settings)
+	usr, err := user.Current()
     if err != nil {
-		log.Fatal("json.Unmarshal", err)
+        log.Error("user.Current: %v", err)
+    }
+	
+	settings := Settings{}
+	settings.Interval = 5
+	settings.Username = ""
+	settings.Password = ""
+	settings.Id = 0
+	settings.Ip = ""
+
+    filePath := fmt.Sprintf("%s/.dnsmadeeasy", usr.HomeDir)
+	if _, err := os.Stat(filePath); err == nil {
+		file, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			log.Error("ioutil.ReadFile: %v", err)
+		}    
+
+	    err = json.Unmarshal(file, &settings)
+	    if err != nil {
+			log.Error("json.Unmarshal: %v", err)
+		}
 	}
+
 	return settings
 }
 
 func saveSettings(settings Settings) {
 	outfile, err := json.MarshalIndent(settings, "", "  ")
     if err != nil {
-		log.Fatal("json.Marshal", err)
+		log.Error("json.Marshal: %v", err)
 	}
-    err = ioutil.WriteFile("settings.json", outfile, 0644)
+
+	usr, err := user.Current()
     if err != nil {
-        fmt.Printf("ioutil.WriteFile: %+v", err)
+        log.Error("user.Current: %v", err)
+    }
+
+    err = ioutil.WriteFile(fmt.Sprintf("%s/.dnsmadeeasy", usr.HomeDir), outfile, 0644)
+    if err != nil {
+        log.Error("ioutil.WriteFile: %v", err)
     }
 }
 
